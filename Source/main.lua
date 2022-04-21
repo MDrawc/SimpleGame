ee10bc5f4e3c480ebfdd52075edbdef1b15e7c27
Michał Drawc <michaldrawc@michas-mac-mini.local>
2022-04-21 10:22:28 +0000

    Add folders to the project structure.

--- Source/main.lua
+++ Source/main.lua
@@ -1,1 +1,15 @@
-//hi
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local playerSprite = nil
+
+
+local function initialize()
+
+end
+
+function playdate.update()
+	
+end
