3f502a7ba3bda88d4e11ee62f9c8c56f60e97532
Michał Drawc <michaldrawc@michas-mac-mini.local>
2022-04-21 13:25:25 +0000

    Move character

--- Source/main.lua
+++ Source/main.lua
@@ -5,6 +5,7 @@ import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
 local playerSprite = nil
+local playerSpeed = 4
 
 
 local function initialize()
@@ -40,5 +41,34 @@ end
 initialize()
 
 function playdate.update()
+	
+	if playdate.buttonIsPressed(playdate.kButtonUp) then
+		playerSprite:moveBy(0, -playerSpeed)
+	end
+	
+	if playdate.buttonIsPressed(playdate.kButtonRight) then
+		playerSprite:moveBy(playerSpeed, 0)
+	end
+	
+	if playdate.buttonIsPressed(playdate.kButtonDown) then
+		playerSprite:moveBy(0, playerSpeed)
+	end
+	
+	if playdate.buttonIsPressed(playdate.kButtonLeft) then
+		playerSprite:moveBy(-playerSpeed, 0)
+	end
+	
+	playdate.timer.updateTimers()
 	gfx.sprite.update() -- update everything in a draw list
 end
+
+
+
+
+
+
+
+
+
+
+
