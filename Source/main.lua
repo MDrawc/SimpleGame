e4fa4f456934488dd381eed87e6f3ec8c9b1f6f4
Michał Drawc <michaldrawc@michas-mac-mini.local>
2022-04-21 13:10:02 +0000

    Change player sprite and display. Add background.

--- Source/main.lua
+++ Source/main.lua
@@ -3,13 +3,27 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+local gfx <const> = playdate.graphics
 local playerSprite = nil
 
 
 local function initialize()
-
+	local playerImage = gfx.image.new("images/player") -- loading an image
+	playerSprite = gfx.sprite.new(playerImage)
+	playerSprite:moveTo(200, 120) -- starts in top-left corner
+	-- ':' because we use moveTo on a particular instance of a sprite !!!
+	playerSprite:add() -- adds sprite to a draw list
 end
 
+--[[
+this 
+is 
+a
+comment
+--]]
+
+initialize()
+
 function playdate.update()
-	
+	gfx.sprite.update() -- update everything in a draw list
 end
